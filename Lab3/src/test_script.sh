8ee9c93728fdbb839d1e7601dcaabab697f07e0e
omraheja <om.raheja@colorado.edu>
2019-10-31 01:31:52 -0600

    Parallelised quick sort using OpenMP

---
+++ Lab3/src/test_script.sh
@@ -0,0 +1,6 @@
+shuf -r -n 2000000 -i 0-3000000 > script_test_file.txt
+sort -n script_test_file.txt > script_test_file_sorted.txt
+make
+./mysort script_test_file.txt -o output.txt
+cmp --silent output.txt script_test_file_sorted.txt && echo "Same!" || echo "Different!"
+
