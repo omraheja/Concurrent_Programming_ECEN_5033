9e19a18ea509c78832d990d13d1240fdf53b39d2
omraheja <om.raheja@colorado.edu>
2019-11-01 22:18:54 -0600

    Modifying test script

--- Lab3/src/test_script.sh
+++ Lab3/src/test_script.sh
@@ -1,4 +1,5 @@
-shuf -r -n 2000000 -i 0-3000000 > script_test_file.txt
+#!/bin/bash
+shuf -r -n 2000000 -i 0-10000000 > script_test_file.txt
 sort -n script_test_file.txt > script_test_file_sorted.txt
 make
 ./mysort script_test_file.txt -o output.txt
